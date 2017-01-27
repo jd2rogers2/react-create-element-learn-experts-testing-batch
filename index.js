b9ab9f9fa3b81339685b96a196ddf1b778039958
jd2rogers2 <jd2rogers2@gmail.com>
2017-01-27 11:28:09 -0500

    donzo

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const list =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'My favorite ice cream flavors'),
-    React.createElement('ul', {},
-      [
-        React.createElement('li', { class: 'brown' }, 'Chocolate'),
-        React.createElement('li', { class: 'white' }, 'Vanilla'),
-        React.createElement('li', { class: 'yellow' }, 'Banana')
-      ]
-    ));
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'i am a little paragraph short and stout');
+const list = React.createElement('div', {},
+  React.createElement('h3', {}, 'My favorite ice cream flavors'),
+  React.createElement('ul', {},
+    [
+      React.createElement('li', { className: 'brown', id: 'choc' }, 'Chocolate'),
+      React.createElement('li', { className: 'white' }, 'Vanilla'),
+      React.createElement('li', { className: 'yellow' }, 'Banana')
+    ]
+  ));
+const container = React.createElement('div', {}, [title, paragraph, list]);
 
-ReactDOM.render(list, document.getElementById('main'));
+
+ReactDOM.render(
+  container, 
+  document.getElementById('main')
+);
+
+// ReactDOM.render(list, document.getElementById('main'));
